d36005b61cddc2c6c9c7b4ec065db5df51ae0392
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-22 23:03:23 +0000

    Adding seeds for dishes and restaurants.

---
+++ db/seeds/restaurants.js
@@ -0,0 +1,7 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('restaurants').del()
+    .then(function () {
+      return knex('restaurants').insert({id: '1', restaurant_name: 'House of Noodles', address: '123 Pandora Ave, Victoria, BC, Canada', phone_number: '1-555-555-5431'});
+    });
+};
