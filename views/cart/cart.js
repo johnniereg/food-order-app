484bb8bd546919ded08da01d98db0b4e6678a96c
Johnnie Regalado <johnniereg@gmail.com>
2017-11-23 20:10:19 +0000

    Wrote function to create cart object

--- views/cart/cart.js
+++ views/cart/cart.js
@@ -2,6 +2,51 @@ function clearCart() {
 
 }
 
-function submitCart() {
 
-}
+let shoppingCart = {
+  1: {id: 1, name: 'Dim Sum', price: 1200, quantity: 1},
+  2: {id: 2, name: 'Hot and Sour Soup', price: 1500, quantity: 2},
+  5: {id: 5, name: 'Spring Rolls', price: 1000, quantity: 1}
+};
+
+
+function submitCart(shoppingCart, phoneNumber) {
+
+  let totalPrice = 0;
+  let cartItems = [];
+
+  for (dish in shoppingCart) {
+
+    let dishPrice = shoppingCart[dish].price;
+    let quantity = shoppingCart[dish].quantity;
+
+    // Add up all the cost of items in the cart
+    let dishSubTotal = dishPrice * quantity;
+    console.log(dishSubTotal);
+    totalPrice += dishSubTotal;
+
+    // Collect total appearances of dish IDs
+    for (let i = quantity; i > 0; i--) {
+      cartItems.push(dish);
+    }
+
+  };
+
+  let order = {
+    phone_number: phoneNumber,
+    cost: totalPrice,
+    dishes: cartItems
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "/api/restaurants/1",
+    dataType: "json", // converts result to JSON
+    success: function (result) {
+      renderDishes(result);
+    }
+  });
+
+};
+
+submitCart(shoppingCart, '12508857405');
