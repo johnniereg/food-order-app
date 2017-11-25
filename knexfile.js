061364d1e8a2cecbfd024c13f6d9f5956def0304
Johnnie Regalado <johnniereg@gmail.com>
2017-11-25 23:23:47 +0000

    Added info to production connection

--- knexfile.js
+++ knexfile.js
@@ -24,9 +24,12 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
+      ssl      : process.env.DB_SSL
     },
     pool: {
       min: 2,
