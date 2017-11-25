faba5961e3e5b7dd57faaa6718bbd52dfffe7cf5
Johnnie Regalado <johnniereg@gmail.com>
2017-11-25 23:28:29 +0000

    Another try

--- knexfile.js
+++ knexfile.js
@@ -23,14 +23,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
