da17396483315a3757b1bac33bac0f810030c584
dbaylyuvic <dbayly@uvic.ca>
2017-11-25 12:31:19 -0800

    Added 2 restaurants to restaurant list and added 1 dish each

--- db/seeds/dishes.js
+++ db/seeds/dishes.js
@@ -77,6 +77,23 @@ exports.seed = function(knex, Promise) {
           cost: '1600',
           restaurant_id: 1
         },
+        {
+          id: 10,
+          dish_name: 'Chicago Style Square Deep Dish Pizza',
+          description: 'A staple of michigan italian cuisine, this pizza is 2.5 inches think, covered in alternating layers of cheese, peperoni and salami',
+          photo_url: 'http://www.seriouseats.com/recipes/assets_c/2017/02/20170216-detroit-style-pizza-43-thumb-1500xauto-436479.jpg',
+          cost: '1800',
+          restaurant_id: 2
+        },
+        {
+          id: 11,
+          dish_name: 'Dynamite roll',
+          description: 'Prawn tempura with avacado rolled in seaweed and rice and covered in japanese style mayonnaise. Contains 6 pieces and comes with soy sauce and pickled ginger',
+          photo_url: 'http://cisl.edu/wordpress/wp-content/uploads/2013/05/Dynamite-Roll-JapaneseFoodAdventures.jpg',
+          cost: '800',
+          restaurant_id: 3
+        }
+        
       ]);
     });
 };
--- db/seeds/restaurants.js
+++ db/seeds/restaurants.js
@@ -2,6 +2,25 @@
 exports.seed = function(knex, Promise) {
   return knex('restaurants').del()
     .then(function () {
-      return knex('restaurants').insert({id: 1, restaurant_name: 'House of Noodles', address: '123 Pandora Ave, Victoria, BC, Canada', phone_number: '1-555-555-5431'});
+      return knex('restaurants').insert([
+      {  
+        id: 1,
+        restaurant_name: 'House of Noodles',
+        address: '123 Pandora Ave, Victoria, BC, Canada',
+        phone_number: '1-555-555-5431'
+      },
+      {  
+        id: 2,
+        restaurant_name: 'Pablos Pizzaria',
+        address: '321 Quadra Street, Victoria, BC, Canada',
+        phone_number: '1-555-555-1111'
+      },
+      {  
+        id: 3,
+        restaurant_name: 'The Sushi bar',
+        address: '359 Douglas Street, Victoria, BC, Canada',
+        phone_number: '1-555-555-3214'
+      }]
+      );
     });
 };
