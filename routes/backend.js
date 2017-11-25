92f5b856e1e9d1501c33147d4319e699cad9393e
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-25 22:26:58 +0000

    Minor refactors.

--- routes/backend.js
+++ routes/backend.js
@@ -3,6 +3,7 @@ const express = require('express');
 module.exports = function(dbHelpers) {
 
   const router = new express.Router();
+  
   router.get('/home', (req, res) => {
     dbHelpers.get_orders(1)
       .then( orders => {
