1bfb2801410a050287e3f34a9787e04db3e9e8a2
dbaylyuvic <dbayly@uvic.ca>
2017-11-26 13:34:48 -0800

    added migrate to add char limit to descriptions in dishes, limit set to 1200

---
+++ db/migrations/20171126131701_description_char_limit.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('dishes', function (table) {
+  table.string('description',1200).alter();
+})
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('dishes', function (table) {
+    table.string('description').alter();
+  })
+};
