7638bd92f83842fc275801b91c2eff8dc7fb1f28
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-27 17:13:13 +0000

    Fixing up bugs, styling form submit.

--- public/scripts/backend/app.js
+++ public/scripts/backend/app.js
@@ -3,9 +3,8 @@ $(function(){
 
   // Listener for submission of dish changes.
   $('.dish-editor-form').on('submit', submitChanges);
-
   // Listener for submission of new dishes.
-  $('.dish-add-new').find('form').on('submit', submitNewDish);
+  $('#add-new-dish').on('submit', submitNewDish);
   // show inputs for individual dish properties
   $('main.dish-edit-panel section').on('click', showInput);
 
