d38b41f9c85dedd583387aa84cf228932c7c7dcd
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-27 15:58:17 +0000

    Fixing bug with opening inputs.

--- public/scripts/backend/app.js
+++ public/scripts/backend/app.js
@@ -1,12 +1,11 @@
 // document ready
 $(function(){
 
-  $('.dish-edit').on('click', makeForms);
   // Listener for submission of dish changes.
   $('.modify-dish').find('form').on('submit', submitChanges);
   // Listener for submission of new dishes.
   $('.dish-add-new').find('form').on('submit', submitNewDish);
-
+  // show inputs for individual dish properties
   $('main.dish-edit-panel section').on('click', showInput);
 
 });
