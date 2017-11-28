6c3079ee5a696fb1b7c2fca9fcc0f9b2882d7a24
DevYves <yfreypons@gmail.com>
2017-11-28 07:08:00 +0000

    cleaned up old element class names

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,43 @@
+
 // Front end scripts - must be ES5
+$(document).ready(function () {
 
-$(document).ready(function() {
 
+  // Hides cart if you click anywhere else on the body
+  $('body').on('click', function () {
+    $('div.shopping-cart').slideUp();
+    $('.id-form').slideUp();
 
- //Jquery popup form to enter order id
-  $('#order-link').on('click', (e) => {
-    $('#monkey').slideToggle();
-    return false;
-  }
+  });
 
-// Hides cart if you click anywhere else on the body
+  $('.id-form').on('click', function (e) {
+    e.stopPropagation();
+  });
 
   // Show and hide shopping cart listener.
-  $('#cart-button').on('click', (e) => {
+  $('#cart-button').on('click', function (e) {
     $('div.shopping-cart').slideToggle();
     e.stopPropagation();
   });
 
   // Allows user to click on the shopping cart without invoking the toggle listener
-  $('.shopping-cart').on('click', (e) => {
+  $('.shopping-cart').on('click', function (e) {
     e.stopPropagation();
   });
 
+  $('#order-link').on('click', function (e) {
+    $('.id-form').slideToggle().find("input").focus();
+    e.stopPropagation();
+  });
+
+  $(".id-form .btn").click( function(e) {
+    event.preventDefault();
+    var formData = $('input[name="orderID"]').val()
+    var url = "/orders/" + formData
+    window.location.replace(url);
+});
+
   // Load dishes on page load
   loadDishes();
+
 });
